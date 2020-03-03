1b6f1d63819462d28c2b3dada31682d771b25c90
GARETH O LEARY - STUDENT <G00375321@gmit.ie>
2020-03-03 17:47:06 +0000

    lab6

---
+++ src/app/services/data.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DataService {
+
+  constructor(private http:HttpClient) { }
+
+  GetStudentData():Observable<any>{
+    return this.http.get('https://jsonblob.com/api/jsonblob/65f25c8f-5d6a-11ea-80ec-a59266082be7');
+  }
+
+  GetWeatherData():Observable<any>{
+    return this.http.get('http://api.openweathermap.org/data/2.5/weather?q=Galway&appid=6a66416403ed8e5e6e762cb8c261f303');
+  }
+}
